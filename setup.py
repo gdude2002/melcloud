78b3e98d6211b5503819ddc96d634dfeca9470e3
Gareth Coles <gdude2002@gmail.com>
2018-01-04 20:40:07 +0000

    [0.0.4] - Fix PyPi rendering

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='melcloud',
-    version='0.0.3',
+    version='0.0.4',
     description='MELCloud API library',
     long_description=long_description,
     url='https://github.com/gdude2002/melcloud',
